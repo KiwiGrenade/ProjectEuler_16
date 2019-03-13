49092466a4399913c1cb25d3a2c1f0c015ebeb26
KiwiGrenade <35775359+KiwiGrenade@users.noreply.github.com>
2019-03-13 14:41:04 +0100

    Create Problem_16.cpp

---
+++ Problem_16.cpp
@@ -0,0 +1,10 @@
+/*2 (do potegi) 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 2/\1000?*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[]={50,3,10,7,40,80};
+    cout << arr[4];
+}
